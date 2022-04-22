44a685ea3dc16f6762531e92c3e4d8704dacf540
BadTripp <renato.turetta00@gmail.com>
2022-04-23 00:38:21 +0200

    fix verc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAoyaqNbQaAk2eYDz4NEHh4LolwkV1RxWY",
+    authDomain: "pickx-bc51e.firebaseapp.com",
+    projectId: "pickx-bc51e",
+    storageBucket: "pickx-bc51e.appspot.com",
+    messagingSenderId: "301753854076",
+    appId: "1:301753854076:web:b23b343782642abb0141f9",
+    measurementId: "G-ZL0W0LN6X4"
+  },
 };
